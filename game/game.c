358938beecff7a0746aaa355b81fc4c4b7a7f040
Raul <rbaezramir@miners.utep.edu>
2023-04-18 12:08:35 -0600

    Rocket Ship Prototype

--- game/game.c
+++ game/game.c
@@ -49,13 +49,48 @@ switch_interrupt_handler()
 
 // axis zero for col, axis 1 for row
 
-short drawPos[2] = {1,10}, controlPos[2] = {2, 10};
-short velocity = 1, maxVelocity = 6, colLimits[2] = {1, screenWidth-1}, rowLimits[2] = {1, screenHeight-1};
+short drawPos[2] = {screenWidth/2,screenHeight/2}, controlPos[2] = {screenWidth/2, screenHeight/2-1};
+short velocity = 0, maxVelocity = 6, colLimits[2] = {1, screenWidth-1}, rowLimits[2] = {1, screenHeight-1};
+char direction = 'w';
+short thrust = 0;
 
 void
-draw_ball(int col, int row, unsigned short color)
+draw_ball(int col, int row, int thrust, unsigned short color)
 {
   fillRectangle(col-1, row-1, 3, 3, color);
+  switch (direction) {
+  case 'w':
+    drawPixel(col,row-2,color);
+    if (thrust) {
+      fillRectangle(col-1,row+2, 3,1, COLOR_RED);
+      drawPixel(col,row+3,COLOR_RED);
+    }
+    break;
+  case 'a':
+    drawPixel(col-2,row,color);
+    if (thrust) {
+      fillRectangle(col+2,row-1, 1,3, COLOR_RED);
+      drawPixel(col+3,row,COLOR_RED);
+    }
+    break; 
+  case 's':
+    drawPixel(col,row+2,color);
+    if (thrust) {
+      fillRectangle(col-1,row-2, 3,1, COLOR_RED);
+      drawPixel(col,row-3,COLOR_RED);
+    }
+    break;
+  case 'd':
+    drawPixel(col+2,row,color);
+    if (thrust) {
+      fillRectangle(col-2,row-1, 1,3, COLOR_RED);
+      drawPixel(col-3,row,COLOR_RED);
+    }
+    break;
+  default:
+    drawPixel(col,row,color);
+    break;
+  }
 }
 
 void
@@ -66,75 +101,118 @@ screen_update_ball()
       goto redraw;
   return;			/* nothing to do */
  redraw:
-  draw_ball(drawPos[0], drawPos[1], COLOR_BLACK); /* erase */
+  fillRectangle(drawPos[0]-3,drawPos[1]-3, 7,7, COLOR_BLACK); /* erase */
   for (char axis = 0; axis < 2; axis ++) 
     drawPos[axis] = controlPos[axis];
-  draw_ball(drawPos[0], drawPos[1], COLOR_WHITE); /* draw */
+  draw_ball(drawPos[0], drawPos[1], thrust, COLOR_WHITE); /* draw */
 }
   
 
 short redrawScreen = 1;
 u_int controlFontColor = COLOR_GREEN;
-char direction;
 
 void wdt_c_handler()
 {
   static int secCount = 0;
-
+  short oldCol = controlPos[0];
+  short newCol = oldCol;
+  short oldRow = controlPos[1];
+  short newRow = oldRow;
   secCount ++;
   if (secCount >= 25) {		/* 10/sec */
-   
-    {				/* move ball */
-      short oldCol = controlPos[0];
-      short newCol = oldCol;
-      short oldRow = controlPos[1];
-      short newRow = oldRow;
-      switch (direction) {
-      case 'w':
-	newRow -= velocity;
-	break;
-      case 'a':
-	newCol -= velocity;
-	break;
-      case 's':
-	newRow += velocity;
-	break;
-      case 'd':
-	newCol += velocity;
-	break;
-      default:
-	velocity = velocity;
-	break;
-      }
-      
-      if (switches & SW1) newCol -= velocity;
-      if (switches & SW4) newCol += velocity;
-      if (switches & SW3) newRow -= velocity;
-      if (switches & SW2) newRow += velocity;
-      
-      if (newCol <= colLimits[0] || newCol >= colLimits[1])
-	newCol = oldCol;
-      if (newRow <= rowLimits[0] || newRow >= rowLimits[1])
-	newRow = oldRow;
-      else {
-	controlPos[0] = newCol;
-	controlPos[1] = newRow;
-      }
+    /* Moves forward and accelerates */
+    if (switches & SW2) {
+      accelerate(&newCol,&newRow,&velocity);
+      thrust = 1;
     }
+    /* Moves forward and decelerates */
+    if (!(switches & SW2) && velocity >= 0) {
+      accelerate(&newCol,&newRow,&velocity);
+      thrust = 0;
+    }
+    if(switches & SW1) turnLeft();
+    if(switches & SW3) turnRight();
     {
-      if (step <= 5)
+      if (step <= 2)
 	step ++;
       else {
 	step = 0;
-	if (!(switches & 15)) velocity = 0;
-	if (velocity <= maxVelocity) velocity++;
+	if ((switches & SW2) && velocity <= maxVelocity) velocity++;
+	if (!(switches & SW2) && velocity > 0) velocity--;
       }
       secCount = 0;
     }
     redrawScreen = 1;
   }
+
+  if (newCol <= colLimits[0] || newCol >= colLimits[1]) newCol = oldCol;
+  if (newRow <= rowLimits[0] || newRow >= rowLimits[1]) newRow = oldRow;
+  else {
+    controlPos[0] = newCol;
+    controlPos[1] = newRow;
+  }
 }
-  
+
+void accelerate(short *newCol, short *newRow, short *velocity) {
+  switch (direction) {
+  case 'w':
+    *newRow -= *velocity;
+    break;
+  case 'a':
+    *newCol -= *velocity;
+    break;
+  case 's':
+    *newRow += *velocity;
+    break;
+  case 'd':
+    *newCol += *velocity;
+    break;
+  default:
+    velocity = velocity;
+    break;
+  }
+}
+
+void turnLeft() {
+  switch (direction) {
+  case 'w':
+    direction = 'a';
+    break;
+  case 'a':
+    direction = 's';
+    break;
+  case 's':
+    direction = 'd';
+    break;
+  case 'd':
+    direction = 'w';
+    break;
+  default:
+    direction = direction;
+    break;
+  }
+}
+
+void turnRight() {
+  switch (direction) {
+  case 'w':
+    direction = 'd';
+    break;
+  case 'a':
+    direction = 'w';
+    break;
+  case 's':
+    direction = 'a';
+    break;
+  case 'd':
+    direction = 's';
+    break;
+  default:
+    direction = direction;
+    break;
+  }
+}
+
 void update_shape();
 
 void main()
