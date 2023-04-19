8b8f03f36f0e2927a9ae2624ac4454be95b77e6c
Raul <rbaezramir@miners.utep.edu>
2023-04-19 14:14:53 -0600

    Walls now teleport to opposite wall

--- game/game.c
+++ game/game.c
@@ -50,13 +50,13 @@ switch_interrupt_handler()
 // axis zero for col, axis 1 for row
 
 short drawPos[2] = {screenWidth/2,screenHeight/2}, controlPos[2] = {screenWidth/2, screenHeight/2-1};
-short maxVelocity = 4, velocity[4] = {0,0,0,0};
-short colLimits[2] = {1, screenWidth-1}, rowLimits[2] = {1, screenHeight-1};
+short maxVelocity = 3, velocity[4] = {0,0,0,0};
+short colLimits[2] = {-2, screenWidth+2}, rowLimits[2] = {-2, screenHeight+2};
 short direction = 0;
 short thrust = 0;
 
 void
-draw_ball(int col, int row, int thrust, unsigned short color)
+draw_Rocket(int col, int row, int thrust, unsigned short color)
 {
   fillRectangle(col-1, row-1, 3, 3, color);
   switch (direction) {
@@ -95,7 +95,7 @@ draw_ball(int col, int row, int thrust, unsigned short color)
 }
 
 void
-screen_update_ball()
+screen_update_Rocket()
 {
   for (char axis = 0; axis < 2; axis ++) 
     if (drawPos[axis] != controlPos[axis]) /* position changed? */
@@ -105,7 +105,7 @@ screen_update_ball()
   fillRectangle(drawPos[0]-3,drawPos[1]-3, 7,7, COLOR_BLACK); /* erase */
   for (char axis = 0; axis < 2; axis ++) 
     drawPos[axis] = controlPos[axis];
-  draw_ball(drawPos[0], drawPos[1], thrust, COLOR_WHITE); /* draw */
+  draw_Rocket(drawPos[0], drawPos[1], thrust, COLOR_WHITE); /* draw */
 }
   
 
@@ -121,47 +121,58 @@ void wdt_c_handler()
   short newRow = oldRow;
   
   secCount ++;
-  if (secCount >= 12) {		/* 10/sec */
-    thrust = (switches & SW2);
+  if (secCount >= 10) {		/* 10/sec */
+    
+    thrust = (switches & SW2);	/* Allows draw_Rocket to add engine thrust */
     accelerate(&newCol,&newRow,&velocity);
-    
-    
-    {
-      if (step <= 1)
-	step ++;
-      else {
-	step = 0;
-	if(switches & SW1) turnLeft();
-	if(switches & SW3) turnRight();
-	if (switches & SW2) {
-	  if (direction <= 1 && (velocity[direction] >= (maxVelocity*-1))) velocity[direction]--;
-	  if (direction >= 2 && (velocity[direction] <= maxVelocity)) velocity[direction]++;
-	}
-	if (!(switches & SW2) && velocity[direction] != 0) {
-	  if (direction <= 1) velocity[direction]++;
-	  else velocity[direction]--;
-	}
-	if (direction != 0 && velocity[0] != 0) velocity[0]++;
-	if (direction != 1 && velocity[1] != 0) velocity[1]++;
-	if (direction != 2 && velocity[2] != 0) velocity[2]--;
-	if (direction != 3 && velocity[3] != 0) velocity[3]--;
+
+    if (step <= 1)
+      step ++;
+    else {
+      step = 0;
+      if(switches & SW1) turnLeft();
+      if(switches & SW3) turnRight();
+      
+      if (switches & SW2) {
+	if (direction <= 1 && (velocity[direction] >= (maxVelocity*-1))) velocity[direction]--;
+	if (direction >= 2 && (velocity[direction] <= maxVelocity)) velocity[direction]++;
       }
-      secCount = 0;
+      decelerate();
     }
+    secCount = 0;
     redrawScreen = 1;
   }
-
-  if (newCol <= colLimits[0] || newCol >= colLimits[1]) newCol = oldCol;
-  if (newRow <= rowLimits[0] || newRow >= rowLimits[1]) newRow = oldRow;
-  else {
-    controlPos[0] = newCol;
-    controlPos[1] = newRow;
-  }
+  updatePos(&oldCol,&newCol,&oldRow,&newRow);
 }
 
+/* Checks if new Pos is within limits, teleports to opposite edge if not */
+void updatePos(short *oldCol, short *newCol, short *oldRow, short *newRow) {
+  if (*newCol <= colLimits[0]) *newCol = colLimits[1]-1;
+  else if (*newCol >= colLimits[1]) *newCol = colLimits[0]+1;
+  
+  if (*newRow <= rowLimits[0]) *newRow = rowLimits[1]-1;
+  else if (*newRow >= rowLimits[1]) *newRow = rowLimits[0]+1;
+  
+  controlPos[0] = *newCol;
+  controlPos[1] = *newRow;
+}
+
+/* Updates x,y according to velocity vectors */
 void accelerate(short *newCol, short *newRow, short *velocity) {
   *newRow += (velocity[0] + velocity[2]);
   *newCol += (velocity[1] + velocity[3]);
+}
+
+/* Reduces velocity in direction which the rocket is not currently accelerating */
+void decelerate() {
+  if (!(switches & SW2) && velocity[direction] != 0) {
+    if (direction <= 1) velocity[direction]++;
+    else velocity[direction]--;
+  }
+  if (direction != 0 && velocity[0] != 0) velocity[0]++;
+  if (direction != 1 && velocity[1] != 0) velocity[1]++;
+  if (direction != 2 && velocity[2] != 0) velocity[2]--;
+  if (direction != 3 && velocity[3] != 0) velocity[3]--;
 }
 
 void turnLeft() {
@@ -193,7 +204,7 @@ void main()
     if (redrawScreen) {
       redrawScreen = 0;
       // update_shape();
-      screen_update_ball();
+      screen_update_Rocket();
     }
     P1OUT &= ~LED;	/* led off */
     or_sr(0x10);	/**< CPU OFF */
@@ -205,7 +216,7 @@ void main()
 void
 update_shape()
 {
-  screen_update_ball();
+  screen_update_Rocket();
 }
 
 void
